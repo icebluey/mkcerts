542de9ec3aa5ffc74c8f4d9398e1a2aa1540eb6c
Kenneth Davidson <xylene2016@protonmail.com>
2025-01-08 11:02:45 -0500

    Update mkRootCA.sh

--- 3layers/mkRootCA.sh
+++ 3layers/mkRootCA.sh
@@ -2,26 +2,29 @@
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 TZ='UTC'; export TZ
 
+[ -f openssl ] && _OPENSSL_BIN='./openssl'
+
+${_OPENSSL_BIN:-openssl}
 umask 022
 set -e
 
 mkdir -p root/private root/certs
 
 # RSA4096 Root Key
-openssl genrsa -out root/private/rootCA.key 4096
+${_OPENSSL_BIN:-openssl} genrsa -out root/private/rootCA.key 4096
 
 # RSA2048 Root Key
-#openssl genrsa -out root/private/rootCA.key 2048
+#${_OPENSSL_BIN:-openssl} genrsa -out root/private/rootCA.key 2048
 
 # ECC P384 Root Key
-#openssl ecparam -genkey -noout -name P-384 -out root/private/rootCA.key
+#${_OPENSSL_BIN:-openssl} ecparam -genkey -noout -name P-384 -out root/private/rootCA.key
 
 # Encrypted Root Key
-#openssl genrsa -aes256 -out root/private/rootCA.key 4096
+#${_OPENSSL_BIN:-openssl} genrsa -aes256 -out root/private/rootCA.key 4096
 
-#openssl ecparam -genkey -noout -name P-384 | openssl ec -aes256 -out root/private/rootCA.key
+#${_OPENSSL_BIN:-openssl} ecparam -genkey -noout -name P-384 | openssl ec -aes256 -out root/private/rootCA.key
 # or
-#openssl ecparam -genkey -noout -name P-384 | openssl pkey -aes256 -out root/private/rootCA.key
+#${_OPENSSL_BIN:-openssl} ecparam -genkey -noout -name P-384 | openssl pkey -aes256 -out root/private/rootCA.key
 
 rm -f /tmp/openssl_rootCA.cnf
 sleep 1
@@ -48,7 +51,7 @@ sleep 1
 _digest_algo='sha384'
 #_digest_algo='sha256'
 
-openssl req -new -${_digest_algo} -config /tmp/openssl_rootCA.cnf \
+${_OPENSSL_BIN:-openssl} req -new -${_digest_algo} -config /tmp/openssl_rootCA.cnf \
 -key root/private/rootCA.key -out root/certs/rootCA.csr \
 -subj "/C=US/OU=Root CA/CN=Root CA"
 
@@ -56,7 +59,7 @@ echo
 sleep 1
 
 # 30 years, 10950 = 30*365
-openssl x509 -req -${_digest_algo} -days 10950 -extfile /tmp/openssl_rootCA.cnf -extensions v3_ca \
+${_OPENSSL_BIN:-openssl} x509 -req -${_digest_algo} -days 10950 -extfile /tmp/openssl_rootCA.cnf -extensions v3_ca \
 -signkey root/private/rootCA.key -in root/certs/rootCA.csr -out root/certs/rootCA.crt
 
 echo
